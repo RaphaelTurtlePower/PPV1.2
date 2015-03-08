d10884b0ac93f8fdfdceba4ea9c5eddae717164f
Christian Mamuad <cmamuad@paypal.com>
2015-03-08 14:54:30 -0700

    Adding basic pods required

---
+++ PPV1.2/PPV12/Pods/Target Support Files/Pods-PPV12Tests-AFNetworking/Pods-PPV12Tests-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PPV12Tests_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_PPV12Tests_AFNetworking
+@end
---
+++ PPV1.2/PPV12/Pods/Target Support Files/Pods-PPV12Tests-SVPullToRefresh/Pods-PPV12Tests-SVPullToRefresh-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PPV12Tests_SVPullToRefresh : NSObject
+@end
+@implementation PodsDummy_Pods_PPV12Tests_SVPullToRefresh
+@end
